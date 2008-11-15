90cff9199ce299562710175b8ef0fe9cb949a50e
Amadeusz Jasak <alfanick@alfanick-nb.(none)>
2008-11-15 19:15:56 +0100

    Added Haml as main templates engine

--- config/init.rb
+++ config/init.rb
@@ -4,15 +4,15 @@ require 'config/dependencies.rb'
  
 use_orm :datamapper
 use_test :rspec
-use_template_engine :erb
+use_template_engine :haml
  
 Merb::Config.use do |c|
   c[:use_mutex] = false
-  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  c[:session_store] = 'datamapper'  # can also be 'memory', 'memcache', 'container', 'datamapper
   
   # cookie session store configuration
   c[:session_secret_key]  = '06c582f68ac086f4235e119236ec16da2f8d7931'  # required for cookie session store
-  # c[:session_id_key] = '_session_id' # cookie session id key, defaults to "_session_id"
+  c[:session_id_key] = 'irc_logger_session_id' # cookie session id key, defaults to "_session_id"
 end
  
 Merb::BootLoader.before_app_loads do
