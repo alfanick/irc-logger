7f3f45d4a2161665ab83cb5bcd1feffdff5858fb
Amadeusz Jasak <amadeusz.jasak@gmail.com>
2008-11-19 21:14:34 +0100

    Added format_message and talk_with helpers. Added related messages and surroundings finding. Create example view.

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,5 +1,25 @@
 module Merb
   module MessagesHelper
-
+    def format_message (message)
+      if message.event == :message
+        '<code class="irc message">' + h("<#{message.guy.nickname}> #{message.content}") + '</code>'
+      else
+        '<code class="irc notice">' + h("*** #{message.content}") + '</code>'
+      end
+    end
+  
+    def talk_with (message, n=5)
+      str = '<ol class="irc talk">'
+      message.with_surroundings(n).each do |msg|
+        if msg == message
+          str << '<li class="result"><strong>' << format_message(msg) << '</strong></li>'
+        else
+          str << "<li>" << format_message(msg) << "</li>"
+        end
+      end
+      str << '</ol>'
+      
+      str
+    end
   end
 end # Merb
--- app/models/message.rb
+++ app/models/message.rb
@@ -9,4 +9,16 @@ class Message
 
   belongs_to :guy
   belongs_to :channel
+  
+  def related(n)
+    if n > 0
+      channel.messages.all(:limit => n, :created_at.gt => created_at)
+    else
+      channel.messages.all(:limit => n.abs, :order => [:created_at.desc], :created_at.lt => created_at).reverse
+    end
+  end
+  
+  def with_surroundings(n=5)
+    related(-n) + [self] + related(n)
+  end
 end
