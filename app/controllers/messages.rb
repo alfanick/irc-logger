24d0467b5ec24f4201be6b77353238351bb15935
Amadeusz Jasak <alfanick@workstation.alfanick.biz>
2008-11-30 19:00:26 +0100

    Removed some generated actions, added simple messages searching.

--- app/controllers/messages.rb
+++ app/controllers/messages.rb
@@ -2,10 +2,10 @@ class Messages < Application
   provides :xml, :yaml, :js
 
   def index
-    if not params['query'].empty?
-      @messages = Message.search(:conditions => [params['query']])
+    if params.include? 'query' and not params['query'].empty?
+      @messages = Message.search(:conditions => ['-event 3|4|5', params['query']], :limit => 10).reverse
     else
-      @messages = Message.all
+      @messages = Message.all(:limit => 20).reverse
     end
     display @messages
   end
@@ -15,48 +15,4 @@ class Messages < Application
     raise NotFound unless @message
     display @message
   end
-
-  def new
-    only_provides :html
-    @message = Message.new
-    display @message
-  end
-
-  def edit(id)
-    only_provides :html
-    @message = Message.get(id)
-    raise NotFound unless @message
-    display @message
-  end
-
-  def create(message)
-    @message = Message.new(message)
-    if @message.save
-      redirect resource(@message), :message => {:notice => "Message was successfully created"}
-    else
-      message[:error] = "Message failed to be created"
-      render :new
-    end
-  end
-
-  def update(id, message)
-    @message = Message.get(id)
-    raise NotFound unless @message
-    if @message.update_attributes(message)
-       redirect resource(@message)
-    else
-      display @message, :edit
-    end
-  end
-
-  def destroy(id)
-    @message = Message.get(id)
-    raise NotFound unless @message
-    if @message.destroy
-      redirect resource(:messages)
-    else
-      raise InternalServerError
-    end
-  end
-
 end # Messages
