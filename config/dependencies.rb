2e70b6c2803cbb4748e1a1960b133d0849c16c93
Amadeusz Jasak <amadeusz.jasak@gmail.com>
2008-12-13 13:11:22 +0100

    Moved to Merb 1.0.4 and DataMapper 0.9.8. Removed bug with dm-sphinx-adapter dependency.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.1"
-dm_gems_version   = "0.9.7"
+merb_gems_version = "1.0.4"
+dm_gems_version   = "0.9.8"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -22,8 +22,8 @@ dependency "dm-timestamps", dm_gems_version
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
 dependency "dm-is-searchable"
-gem "shanna-dm-sphinx-adapter"
-dependency "dm-sphinx-adapter"
+#gem "shanna-dm-sphinx-adapter"
+require "dm-sphinx-adapter"
 
 dependency "haml"
 
