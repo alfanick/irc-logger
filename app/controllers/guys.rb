688fb8ee67312fe30c04d9c73aed2e0a79594ab5
Amadeusz Jasak <amadeusz.jasak@gmail.com>
2008-12-15 09:53:44 +0100

    Removed unneccesary guys actions

--- app/controllers/guys.rb
+++ app/controllers/guys.rb
@@ -11,48 +11,4 @@ class Guys < Application
     raise NotFound unless @guy
     display @guy
   end
-
-  def new
-    only_provides :html
-    @guy = Guy.new
-    display @guy
-  end
-
-  def edit(id)
-    only_provides :html
-    @guy = Guy.get(id)
-    raise NotFound unless @guy
-    display @guy
-  end
-
-  def create(guy)
-    @guy = Guy.new(guy)
-    if @guy.save
-      redirect resource(@guy), :message => {:notice => "Guy was successfully created"}
-    else
-      message[:error] = "Guy failed to be created"
-      render :new
-    end
-  end
-
-  def update(id, guy)
-    @guy = Guy.get(id)
-    raise NotFound unless @guy
-    if @guy.update_attributes(guy)
-       redirect resource(@guy)
-    else
-      display @guy, :edit
-    end
-  end
-
-  def destroy(id)
-    @guy = Guy.get(id)
-    raise NotFound unless @guy
-    if @guy.destroy
-      redirect resource(:guys)
-    else
-      raise InternalServerError
-    end
-  end
-
 end # Guys
