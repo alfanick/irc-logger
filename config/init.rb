811b68da21ea53caf91a594a009959abc1af5dd7
Amadeusz Jasak <amadeusz.jasak@gmail.com>
2008-12-16 20:14:30 +0100

    Removed bug with cache initializing

--- config/init.rb
+++ config/init.rb
@@ -24,7 +24,7 @@ Merb::BootLoader.before_app_loads do
   # This will get executed after dependencies have been loaded but before your app's classes have loaded.
   DataMapper.setup(:search, 'sphinx://localhost/./config/sphinx.conf')
   Merb::Cache.setup do
-    register(:memory_store, Merb::Cache::MemcachedStore, :namespace => "irc-logger", :servers => Merb::Config[:cache]["servers"]) if not exists(:memcached)
+    register(:memory_store, Merb::Cache::MemcachedStore, :namespace => "irc-logger", :servers => Merb::Config[:cache]["servers"]) if not exists?(:memcached)
 #    register(:page_store, Merb::Cache::PageStore[Merb::Cache::FileStore], :dir => Merb.root / "public")
     register(:default, Merb::Cache::AdhocStore[:memory_store]) if not exists(:default)
 
