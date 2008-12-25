c596fe27e41d6c60933af1b4aa8b909498cc28a6
Amadeusz Jasak <amadeusz.jasak@gmail.com>
2008-12-25 16:06:37 +0100

    Moved to Merb 1.0.6 and DataMapper 0.9.9

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.4"
-dm_gems_version   = "0.9.8"
+merb_gems_version = "1.0.6"
+dm_gems_version   = "0.9.9"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -21,7 +21,7 @@ dependency "dm-migrations", dm_gems_version
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
 dependency "dm-validations", dm_gems_version  
-dependency "dm-is-searchable"
+#dependency "dm-is-searchable"
 #gem "shanna-dm-sphinx-adapter"
 require "dm-sphinx-adapter"
 
