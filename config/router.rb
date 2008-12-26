cd7af9580568bcc3de456bf7861e5070d55ce02f
Amadeusz Jasak <amadeusz.jasak@gmail.com>
2008-12-26 14:07:42 +0100

    Removed bug - messages routes had lower priority than server... Added query building for Guy, Server and Channels

--- config/router.rb
+++ config/router.rb
@@ -1,17 +1,17 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
-  match('/guys/:page').to(:controller => 'guys', :action => 'index').name(:guys)
-  match('/guy/:nickname').to(:controller => 'guys', :action => 'show').name(:show_guy)
+	match('/messages/:page').to(:controller => 'messages', :action => 'index').name(:search)
+  match('/message/:id/:bcount/:count').to(:controller => 'messages', :action => 'show').name(:show_message)
+  match('/logs/:host/:channel/:year/:month/:day', :host=>/[a-z.0-9_]+/i).to(:controller => 'messages', :action => 'log', :format=>'log').name(:raw_log)
 
-	match('/:server/channels/:page', :server=>/[a-z.0-9_]+/i).to(:controller => 'channels', :action => 'index').name(:channels)
-	match('/:server/:name', :server=>/[a-z.0-9_]+/i).to(:controller => 'channels', :action => 'show').name(:show_channel)
+  match('/guys/:page').to(:controller => 'guys', :action => 'index').name(:guys)
+  match('/guy/:nickname').to(:controller => 'guys', :action => 'show', :query => '@nickname [1]').name(:show_guy)
+
+	match('/:server/channels/:page', :server=>/[a-z.0-9_]+/i).to(:controller => 'channels', :action => 'index', :query => '@server [1]').name(:channels)
+	match('/:server/:name', :server=>/[a-z.0-9_]+/i).to(:controller => 'channels', :action => 'show', :query => '@channel #[2] @server [1]').name(:show_channel)
   
 	match('/servers').to(:controller => 'servers', :action => 'index').name(:servers)
 
-  match('/messages/:page').to(:controller => 'messages', :action => 'index').name(:search)
-  match('/message/:id/:bcount/:count').to(:controller => 'messages', :action => 'show').name(:show_message)
-  match('/logs/:host/:channel/:year/:month/:day', :host=>/[a-z.0-9_]+/i).to(:controller => 'messages', :action => 'log', :format=>'log').name(:raw_log)
-  
   slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
 
   default_routes
